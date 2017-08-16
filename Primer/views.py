1ecd55271fe23850006a9fe3b2d85d71b42c79e3
junior <freesoftandskate@gmail.com>
2017-08-16 01:54:24 -0500

    Mostrar las areas en la capa view

--- Primer/views.py
+++ Primer/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.core.serializers import serialize
 from django.http import HttpResponse
+from . import models
 
 
-def index(request):
-    return render(request, 'Primer/index.html', {})
+class index(TemplateView):
+    template_name = 'Primer/index.html'
+
+
+def sectores(request):
+    sector = serialize('geojson', models.Sector.objects.all())
+    return HttpResponse(sector, content_type='json')
