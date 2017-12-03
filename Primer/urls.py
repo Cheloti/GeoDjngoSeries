f1ba3960e14c5d067492c0b01d1212d58b2f33ba
yunica <freesoftandskate@gmail.com>
2017-12-02 22:14:38 -0500

    line o multi punto

--- Primer/urls.py
+++ Primer/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index.as_view(), name='index'),
     url(r'^sectoresbd/$', views.sectores, name='sectoressbd'),
-    url(r'^confe/$', views.prueba, name='mencionar'),
+    url(r'^confe/$', views.mencionar, name='mencionar'),
 ]
--- Primer/views.py
+++ Primer/views.py
@@ -12,7 +12,7 @@ from django.contrib.gis.geos import GEOSGeometry
 import os
 from django.conf import settings
 
-path = os.path.join(settings.BASE_DIR, 'Primer/incidentes _modificado.csv')
+path = os.path.join(settings.BASE_DIR, 'static/inc2.csv')
 
 
 class index(TemplateView):
@@ -20,60 +20,57 @@ class index(TemplateView):
 
 
 def sectores(request):
-    obj = serialize('geojson', models.Incidences.object.all())
+    obj = serialize('geojson', DelitosMuni.object.filter(referenciado=True))
     return HttpResponse(obj, content_type='json')
 
 
 def mencionar(request):
-    try:
-        url = path
-        with open(url) as csvarchivo:
-            entrada = csv.reader(csvarchivo)
-            # DELITO, DIRECCION, CANTIDAD, ZONA, SUB_ZONA, SUB_ZONA_1, SUB_ZONA_2, MES, SUBURB = next(entrada)
-            next(entrada)
-            actual = date.today()
-            geolocator = Nominatim()
-            for i in entrada:
-                idfalso, DELITO, DIRECCION, CANTIDAD, ZONA, SUB_ZONA, SUB_ZONA_1, SUB_ZONA_2, MES, SUBURB = i
-                location = False
-                try:
-                    location = geolocator.geocode(str(DIRECCION + " , San Juan Bautista "), timeout=10)
-                except Exception:
-                    print('error en el location')
+    url = path
+    with open(url) as csvarchivo:
+        entrada = csv.reader(csvarchivo)
+        # DELITO, DIRECCION, CANTIDAD, ZONA, SUB_ZONA, SUB_ZONA_1, SUB_ZONA_2, MES, SUBURB = next(entrada)
+        next(entrada)
+        geolocator = Nominatim()
+        for i in entrada:
+            idfalso, DELITO, DIRECCION, CANTIDAD, ZONA, SUB_ZONA, SUB_ZONA_1, SUB_ZONA_2, MES, SUBURB = i
+            try:
+                location = geolocator.geocode(str(DIRECCION + " , San Juan Bautista "), timeout=5)
+            except Exception:
+                print("Direccion no encontrada " + DIRECCION)
 
-                # location = geolocator.geocode(str(DIRECCION + " , San Juan Bautista "), timeout=10)
-                for j in range(0, int(CANTIDAD)):
-                    delitomuni = DelitosMuni(
-                        idfalso=int(idfalso),
-                        delito=str(DELITO),
-                        direccion=str(DIRECCION),
-                        cantidad=int(CANTIDAD),
-                        zona=str(ZONA),
-                        subzona=str(SUB_ZONA),
-                        subzona1=str(SUB_ZONA_1),
-                        subzona2=str(SUB_ZONA_2),
-                        mes=date(year=2017, month=int(MES), day=2))
+            # location = geolocator.geocode(str(DIRECCION + " , San Juan Bautista "), timeout=10)
+            for j in range(0, int(CANTIDAD)):
+                delitomuni = DelitosMuni(
+                    idfalso=int(idfalso),
+                    delito=str(DELITO),
+                    direccion=str(DIRECCION),
+                    cantidad=int(CANTIDAD),
+                    zona=str(ZONA),
+                    subzona=str(SUB_ZONA),
+                    subzona1=str(SUB_ZONA_1),
+                    subzona2=str(SUB_ZONA_2),
+                    mes=date(year=2017, month=int(MES), day=2))
 
-                    if location:
-                        delitomuni.lat = float(location.latitude)
-                        delitomuni.lng = float(location.longitude)
-                        delitomuni.punto = GEOSGeometry(
-                            'POINT(%s %s), 4326' % (
-                                float(location.longitude), float(location.latitude)))
-                        delitomuni.nuevadireccion = str(location.address)
-                        delitomuni.referenciado = True
-                    else:
-                        delitomuni.referenciado = False
+                if location:
+                    delitomuni.lat = float(location.latitude)
+                    delitomuni.lng = float(location.longitude)
+                    delitomuni.punto = GEOSGeometry(
+                        'POINT(%s %s), 4326' % (
+                            float(location.longitude), float(location.latitude)))
+                    delitomuni.nuevadireccion = str(location.address)
+                    delitomuni.referenciado = True
+                else:
+                    delitomuni.referenciado = False
 
-                    delitomuni.save()
-    except Exception:
-        print('algo salio mal')
+                delitomuni.save()
 
-    return render(request, 'Primer/resultado.html', {'muni': 'helow'})
+    return render(request, 'Primer/resultado.html',
+                  {'muni': DelitosMuni.object.all(),
+                   'sinrefe': DelitosMuni.object.filter(referenciado=False),
+                   'conrefe': DelitosMuni.object.filter(referenciado=True)})
 
 
-def prueba(request):
-    for i in range(0, 9):
-        p = Prueba(mensaje="hola carajo 3")
-        p.save()
-    return render(request, 'Primer/resultado.html', {'muni': 'helow'})
+def prueba():
+    delito = DelitosMuni.object.filter(referenciado=False).order_by('direccion')
+    for i in delito:
+        print(i.direccion)
